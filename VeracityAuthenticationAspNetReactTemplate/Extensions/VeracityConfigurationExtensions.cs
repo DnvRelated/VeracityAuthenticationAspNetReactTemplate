ed518a77aa4cace2b73e80738908a76beda721a7
Klimek, Mariusz <Mariusz.Klimek@dnv.com>
2023-05-31 22:54:44 +0200

    Add Veracity Configuration

---
+++ VeracityAuthenticationAspNetReactTemplate/Extensions/VeracityConfigurationExtensions.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.DataProtection;
+using Microsoft.Extensions.Caching.Distributed;
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Options;
+using Veracity.Common.Authentication;
+using Veracity.Common.OAuth.Providers;
+
+namespace VeracityAuthenticationAspNetReactTemplate.Extensions;
+
+internal static class VeracityConfigurationExtensions
+{
+    public static void AddVeracityConfiguration(this WebApplicationBuilder builder, ConfigurationManager configuration)
+    {
+        builder.Services.AddVeracity(configuration)
+            .Configure<CookiePolicyOptions>(options =>
+            {
+                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            })
+            .AddSingleton(ConstructDataProtector)
+            .AddVeracityServices(configuration["Veracity:MyServicesApi"])
+            .AddSingleton(ConstructDistributedCache)
+            .AddSession()
+            .AddAuthentication(sharedOptions =>
+            {
+                sharedOptions.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                sharedOptions.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
+            })
+            .AddVeracityAuthentication(configuration)
+            .AddCookie();
+    }
+
+    private static IDistributedCache ConstructDistributedCache(IServiceProvider s)
+        => new MemoryDistributedCache(new OptionsWrapper<MemoryDistributedCacheOptions>(new MemoryDistributedCacheOptions()));
+
+    private static Veracity.Common.Authentication.IDataProtector ConstructDataProtector(IServiceProvider s)
+    {
+        return new DataProtector<IDataProtectionProvider>(s.GetDataProtectionProvider(),
+                    (p, data) => p.CreateProtector("token").Protect(data),
+                    (p, data) => p.CreateProtector("token").Unprotect(data));
+    }
+}
--- VeracityAuthenticationAspNetReactTemplate/Program.cs
+++ VeracityAuthenticationAspNetReactTemplate/Program.cs
@@ -1,37 +1,44 @@
+using Veracity.Common.Authentication;
+using VeracityAuthenticationAspNetReactTemplate.Extensions;
 
-namespace VeracityAuthenticationAspNetReactTemplate
+namespace VeracityAuthenticationAspNetReactTemplate;
+
+public class Program
 {
-    public class Program
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
+        var builder = WebApplication.CreateBuilder(args);
+        var configuration = builder.Configuration;
+        builder.Logging.AddConsole();
+
+        // Add services to the container.
+
+        builder.Services.AddControllersWithViews();
+        builder.AddVeracityConfiguration(configuration);
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (!app.Environment.IsDevelopment())
         {
-            var builder = WebApplication.CreateBuilder(args);
-
-            // Add services to the container.
-
-            builder.Services.AddControllersWithViews();
-
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if (!app.Environment.IsDevelopment())
-            {
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
-
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
-            app.UseRouting();
-
-
-            app.MapControllerRoute(
-                name: "default",
-                pattern: "{controller}/{action=Index}/{id?}");
-
-            app.MapFallbackToFile("index.html");
-
-            app.Run();
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            app.UseHsts();
         }
+
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
+        app.UseVeracity();
+        app.UseRouting();
+
+        app.UseAuthentication();
+        app.UseAuthorization();
+
+        app.MapControllerRoute(
+            name: "default",
+            pattern: "{controller}/{action=Index}/{id?}");
+
+        app.MapFallbackToFile("index.html");
+
+        app.Run();
     }
 }
